b5dd859b0d06884d4df622cfabd2928e7f1ee4ba
hilarious3 <shahzad20022002@gmail.com>
2023-10-02 20:19:09 +0530

    backend login and signup api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const authRoutes=require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
@@ -11,5 +11,5 @@ app.use(express.urlencoded());
 app.get("/", (req, res) => {
   res.send("hii World!");
 });
-
+app.use("/auth",authRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
