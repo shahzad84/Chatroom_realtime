6b797ed2cfa8af1db1f44a96f954aca18fdf634d
hilarious3 <shahzad20022002@gmail.com>
2023-10-03 00:18:02 +0530

    implementing logout functionallity

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -4,8 +4,8 @@ import Cookies from "universal-cookie";
 import { ChannelSearch,TeamChannelList,TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png'
 import LogoutIcon from '../assets/logout.png'
-
-const SideBar = () => (
+const cookies = new Cookies();
+const SideBar = ({logout}) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
@@ -13,7 +13,7 @@ const SideBar = () => (
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner">
+            <div className="icon1__inner" onClick={logout}>
                 <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -25,9 +25,20 @@ const CompanyHeader = () => (
     </div>
 )
 const ChannelListContainer = () => {
+    const logout = () => {
+      cookies.remove("token");
+      cookies.remove('userId');
+      cookies.remove('username');
+      cookies.remove('fullName');
+      cookies.remove('avatarURL');
+      cookies.remove('hashedPassword');
+      cookies.remove('phoneNumber');
+
+      window.location.reload();
+    }
   return (
     <>
-      <SideBar/>
+      <SideBar logout={logout} />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch/>
