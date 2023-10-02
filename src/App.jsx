b876a77653dab0e88c8358d123285a7d85f15b03
hilarious3 <shahzad20022002@gmail.com>
2023-10-02 17:58:34 +0530

    making login signup ui in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
-import { ChannelContainer,ChannelListContainer } from './components';
+import { ChannelContainer,ChannelListContainer,Auth } from './components';
 import "./App.css";
 import Cookies from 'universal-cookie';
 const apiKey = '68avshree48h';
 const client = StreamChat.getInstance(apiKey);
-
+const authToken=false;
 const App = () => {
+  if(!authToken) return <Auth/>
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team light" >
