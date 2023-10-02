c5d3864d1ab45afc38cd134a506edb32501edf5b
hilarious3 <shahzad20022002@gmail.com>
2023-10-02 08:02:49 +0530

    making components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import { ChannelContainer,ChannelListContainer } from './components';
+import Cookies from 'universal-cookie';
+const apiKey = '68avshree48h';
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>hellow</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme="team light" >
+        <ChannelListContainer/>
+        <ChannelContainer/>
+        </Chat>
     </div>
   )
 }
---
+++ src/components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelContainer = () => {
+  return (
+    <div>
+      <h1>hii channelcontainer</h1>
+    </div>
+  )
+}
+
+export default ChannelContainer
